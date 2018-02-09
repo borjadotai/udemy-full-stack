b2cf26d3147cb99e365e817af6a9cfff20ec1793
p3rzival <p3rzival@gmail.com>
2018-02-09 15:04:23 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'theruuu'});
-});
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
