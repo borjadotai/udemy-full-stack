a54120ad9e95c8acc545ec7db6a3bf04902b6f43
p3rzival <p3rzival@gmail.com>
2018-02-16 22:48:54 +0000

    tweaking stuff

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose =  require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-authRoutes(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
