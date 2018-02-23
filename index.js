5243caf213111f99ba669d2befe3ce49357c30a6
p3rzival <p3rzival@gmail.com>
2018-02-23 11:30:35 +0000

    trying to fix it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const mongoose =  require('mongoose');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -11,12 +11,11 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
